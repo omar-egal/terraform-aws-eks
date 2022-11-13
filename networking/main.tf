0d673a46e66bad5ed5a547dc53561cc8bef4e7f0
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-11-13 06:57:19 +0000

    Created custom vpc, initial commit

---
+++ networking/main.tf
@@ -0,0 +1,60 @@
+#---networking/main.tf---
+
+# Declare the data source
+data "aws_availability_zones" "available" {}
+
+resource "random_integer" "random" {
+  min = 1
+  max = 100
+}
+
+resource "random_shuffle" "az_list" {
+  input        = data.aws_availability_zones.available.names
+  result_count = var.max_subnets
+}
+
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "week22-vpc-${random_integer.random.id}"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_internet_gateway" "week22_internet_gateway" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "week22-igw"
+  }
+}
+
+resource "aws_subnet" "week22_public_subnet" {
+  count                   = var.public_sn_count
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = var.public_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = random_shuffle.az_list.result[count.index]
+
+  tags = {
+    Name = "week22-public-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "week22_public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "week22-public"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.week22_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.week22_internet_gateway.id
+}
+
---
+++ providers.tf
@@ -0,0 +1,15 @@
+#---root/providers.tf---
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
