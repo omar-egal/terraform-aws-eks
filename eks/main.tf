366b0e2003866de749c738bddd68659a9734cc45
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-11-14 20:23:34 +0000

    Bug fixes

--- eks/main.tf
+++ eks/main.tf
@@ -1,1 +1,17 @@
 #---eks/main.tf---
+
+resource "aws_eks_cluster" "cluster" {
+  name     = "cluster"
+  role_arn = var.role_arn
+
+  vpc_config {
+    subnet_ids = var.public_subnets
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    var.AmazonEKSClusterPolicy,
+    var.AmazonEKSVPCResourceController,
+  ]
+}
