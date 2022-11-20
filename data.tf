35b7b67759c1e2fe01e9c58de033eb76666a9dd3
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-11-20 16:00:56 +0000

    Added node ip outputs

--- data.tf
+++ data.tf
@@ -5,3 +5,11 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
+
+data "aws_instances" "my_worker_nodes" {
+  instance_tags = {
+    "kubernetes.io/cluster/${module.eks.cluster_name}" = "owned"
+  }
+  instance_state_names = ["running"]
+  depends_on           = [module.eks.node_group]
+}
